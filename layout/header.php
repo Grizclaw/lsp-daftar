0f81ed94bedee9ad9c6edd8488000adaafc7a14f
Abyan Putra <81188572+Grizclaw@users.noreply.github.com>
2022-07-04 02:23:39 +0700

    Add files via upload

---
+++ layout/header.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'config.php';
+  
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PENDAFTARAN SISWA BARU SMP 73 JAKARTA SELATAN</title>
+	
+	<link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="bootstrap/js/jquery.min.js"></script>
+	<link rel="stylesheet" href="layout/style.css" />
+	
+</head>
+
+<body>
+
+<nav class="navbar navbar-dark bg-primary">
+	<div class="container">
+	  <a class="navbar-brand">PENDAFTARAN SISWA BARU SMP 73 JAKARTA SELATAN</a>
+	  <a class="btn btn-light btn-md" href="keluar.php" role="button" onclick="return confirm('Anda Yakin Keluar?')"> Logout</a>
+</nav>
